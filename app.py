2da71a39ee499b313d82e3dcb42bf43858e62f8c
hdi <hdi@chemometec.com>
2023-12-04 11:07:09 +0100

    moved all functions out of app to make use of them without sending the response every time

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ import requests
 import json
 import re #regular expression
 
+from src.functions import connectionCheck, getProtocols, getRuns, postRun, getCurrentRun, getRunStatus, postRunAction
+
 app = Flask(__name__)
 CORS(app)
 
@@ -24,27 +26,6 @@ robotPORT = ":31950"
 contentType = 'application/json'
 nodeUrl = 'http://localhost:4000/connect' #Node Server url to check IP address
 
-def connectionCheck():
-    """
-    Function to check if the connection to the robot is working by fetching the IP address from the Node server.
-    It is used by the \connect endpoint."""
-
-    try:
-        robotRequest = requests.get(nodeUrl)
-        if(robotRequest.status_code == 200):
-            result = json.loads(robotRequest.text)
-            IP_ADDRESS = result["data"]
-            match = re.search(r'^((25[0-5]|(2[0-4]|1\d|[1-9]|)\d)(\.(?!$)|$)){4}$', result["data"]) #regular expression for IP address
-            if(match == None):
-                return "False"
-            else:
-                return IP_ADDRESS #IP address found
-        else:
-            return "False"
-    except ConnectionError:
-        return "False"
-    except:
-        return "False"
     
 ### ROUTES ###
 
@@ -60,195 +41,40 @@ def connect():
     """Route to check if the robot is running and if it's IP address is known."""
 
     connected = connectionCheck()
-    return Response('{connected}'.format(connected=connected), status=200, mimetype=contentType)
+    return Response(json.dumps(connected), status=200, mimetype=contentType)
 
 # url to use to get list of protocols known to the robot
 @app.get('/protocols')
-def getProtocols():
-    """Route to get list of protocols known to the robot."""
+def getProtocolsResponse():
+    protocols = getProtocols()
+    return Response(json.dumps(protocols), status=200, mimetype=contentType)
 
-    IP_ADDRESS = connectionCheck()
-    if(IP_ADDRESS == "False"):
-        return Response(json.dumps({'error': 'No connection to robot'}), status=200, mimetype=contentType)
 
-    url = urlStart + IP_ADDRESS + robotPORT + "/protocols"
-    headers = {"opentrons-version": "2", "Content-Type": contentType}
-
-    #robotProtocolRequest
-    try:
-        robotProtocolRequest = requests.request("GET", url, headers=headers)
-    except requests.exceptions.InvalidURL:
-        return Response(json.dumps({'error': 'No connection to robot'}), status=200, mimetype=contentType)
-    protocols = json.loads(robotProtocolRequest.text)
-
-    if(robotProtocolRequest.status_code == 200):
-        return Response(json.dumps(protocols), status=200, mimetype=contentType)
-    else:
-        return Response(json.dumps({'error': 'No protocols found'}), status=200, mimetype=contentType)
+@app.get('/runs')
+def getRunsResponse():
+    runs = getRuns()
+    return Response(json.dumps(runs), status=200, mimetype=contentType)
 
 @app.post('/runs')
-def post_run():
-    """Route to create a run for the robot."""
-    IP_ADDRESS = connectionCheck()
-    if(IP_ADDRESS == "False"):
-        return Response(json.dumps({'error': 'No connection to robot'}), status=200, mimetype=contentType)
-
-    try:
-        obj = request.get_data()
-        obj = json.loads(obj)
-        tempProtocolId = obj['protocol_id']
-    except KeyError:
-        return Response(json.dumps({'error': 'No id found in body request'}), status=200, mimetype=contentType)
-
-    url = urlStart + IP_ADDRESS + robotPORT + "/runs"
-    headers = {"opentrons-version": "2", "Content-Type": contentType}
-    payload = {
-        "data": {
-            "protocolId": "",
-            "labwareOffsets": [
-                {
-                    "definitionUri": "opentrons/opentrons_96_tiprack_300ul/1",
-                    "location": {
-                        "slotName": "1"
-                },
-                "vector": {
-                    "x": 0.29999999999999893,
-                    "y": 0.9000000000000057,
-                    "z": 0.0
-                    }
-                }
-            ]
-        }
-    }
-    payload["data"]["protocolId"] = tempProtocolId
-    payload = json.dumps(payload)
-
-    try:
-        robotRequest = requests.request("POST", url, headers=headers, data=payload)
-    except requests.exceptions.InvalidURL:
-        return Response(json.dumps({'error': 'No connection to robot'}), status=200, mimetype=contentType)
-    
-    if(robotRequest.status_code == 201):
-        createdRunId = robotRequest.json()["data"]["id"]
-        return Response(json.dumps({'message': 'Run created', 'runId': '{createdRunId}'.format(createdRunId=createdRunId)}), status=201, mimetype=contentType)
-    else:
-        return Response(json.dumps({'error': 'Run was not created'}), status=200, mimetype=contentType)
+def postRunResponse():
+    response = postRun()
+    return response
 
 @app.get('/currentRun')
-def getCurrentRun():
-    """Route to get the current run id"""
-    IP_ADDRESS = connectionCheck()
-    if(IP_ADDRESS == "False"):
-        return Response(json.dumps({'error': 'No connection to robot'}), status=200, mimetype=contentType)
-
-    url = urlStart + IP_ADDRESS + robotPORT + "/runs"
-    headers = {"opentrons-version": "2", "Content-Type": contentType}
-    protocolRequest = requests.request("GET", url, headers=headers )
-
-    if(protocolRequest.status_code == 200):
-        protocolRequest = protocolRequest.json()
-    else:
-        return Response(json.dumps({'error': 'No runs found'}), status=200, mimetype=contentType)
-
-    try:
-        tempCurrentRun = protocolRequest["links"]["current"]["href"]
-        tempCurrentRun = tempCurrentRun.split("/runs/")
-        currentRun = tempCurrentRun[1]
-    except KeyError: #this happens if there is no current run
-        currentRun = ""
-
+def getCurrentRunResponse():
+    currentRun = getCurrentRun()
     return Response(json.dumps(currentRun), status=200, mimetype=contentType)
 
 @app.get('/runStatus')
-def runStatus():
-    """Route to get the status of the current run."""
-    IP_ADDRESS = connectionCheck()
-    if(IP_ADDRESS == "False"):
-        return Response(json.dumps({'error': 'No connection to robot'}), status=200, mimetype=contentType)
-
-    currentRunRequest = getCurrentRun()
-    if(currentRunRequest.status_code == 200):
-        currentRun = currentRunRequest.json
-        if(currentRun == ""):
-            return Response(json.dumps({'error': 'No current run'}), status=200, mimetype=contentType)
-
-    url = urlStart + IP_ADDRESS + robotPORT + "/runs/" + currentRun
-    headers = {"opentrons-version": "2", "Content-Type": contentType}
-    
-    try:
-        robotRequest = requests.request("GET", url, headers=headers)
-    except requests.exceptions.InvalidURL:
-        return Response(json.dumps({'error': 'No IP to robot found'}), status=200, mimetype=contentType)
-    except requests.exceptions.ConnectionError:
-        return Response(json.dumps({'error': 'No connection to robot'}), status=200, mimetype=contentType)
-
-    if(robotRequest.status_code == 200):
-        try:
-            status = robotRequest.json()["data"]["status"]
-            return Response(status, status=200, mimetype=contentType)
-        except KeyError:
-            return Response(json.dumps({'error': 'No current run'}), status=200, mimetype=contentType)
-    else:
-        return Response(json.dumps({'error': '{error}'.format(error=robotRequest)}), status=200, mimetype=contentType)
+def getRunStatusResponse():
+    status = getRunStatus()
+    return Response(json.dumps(status), status=200, mimetype=contentType)
 
 #this command is used to run/resume, pause or stop a run
 @app.post('/command')
-def runAction():
-    """Route to run/resume, pause or stop a run."""
-    IP_ADDRESS = connectionCheck()
-    if(IP_ADDRESS == "False"):
-        return Response(json.dumps({'error': 'No connection to robot'}), status=200, mimetype=contentType)
-
-    try:
-        obj = request.get_data()
-        obj = json.loads(obj)
-        command = obj['command']
-    except KeyError:
-        return Response(json.dumps({'error': 'No command found'}), status=200, mimetype=contentType)
-    except requests.exceptions.ConnectionError:
-        return Response(json.dumps({'error': 'No connection to robot'}), status=200, mimetype=contentType)
-
-    if(runStatus() == "succeeded" or runStatus() == "stopped"):
-        return Response(json.dumps({'error': 'Run has already been executed'}), status=200, mimetype=contentType)
-
-    if(command == "play"):
-        payload = {
-            "data": {
-                "actionType": "play" 
-            }
-        }
-    elif(command == 'pause'):
-        payload = {
-            "data": {
-                "actionType": "pause" 
-            }
-        }
-    else:
-        payload = {
-            "data": {
-                "actionType": "stop" 
-            }
-        }
-    payload = json.dumps(payload)
-
-    currentRunRequest = getCurrentRun()
-    if(currentRunRequest.status_code == 200):
-        currentRun = currentRunRequest.json
-        if(currentRun == ""):
-            return Response(json.dumps({'error': 'No current run'}), status=200, mimetype=contentType)
-
-    url = urlStart + IP_ADDRESS + robotPORT + "/runs/" + currentRun + "/actions"
-    headers = {"opentrons-version": "2", "Content-Type": contentType}
-    
-    try:
-        robotRequest = requests.request("POST", url, headers=headers, data=payload)
-    except requests.exceptions.InvalidURL:
-        return Response(json.dumps({'error': 'No connection to robot'}), status=200, mimetype=contentType)
-    
-    if(robotRequest.status_code == 201):
-        return Response(json.dumps({'message': '{command}'.format(command=command)}), status=201, mimetype=contentType)
-    else:
-        return Response(json.dumps({'error': '{error}'.format(error=robotRequest.reason)}), status=200, mimetype=contentType)
+def postRunActionResponse():
+    response = postRunAction()
+    return response
 
 ### Main ###
 if __name__ == '__main__':
---
+++ src/functions.py
@@ -0,0 +1,229 @@
+from flask import Flask, Response, request
+from flask_cors import CORS
+from requests import ConnectionError
+import requests
+import json
+import re #regular expression
+
+urlStart = 'http://'
+robotPORT = ":31950"
+contentType = 'application/json'
+nodeUrl = 'http://localhost:4000/connect' #Node Server url to check IP address
+
+def connectionCheck():
+    """
+    Function to check if the connection to the robot is working by fetching the IP address from the Node server.
+    It is used by the \connect endpoint."""
+
+    try:
+        robotRequest = requests.get(nodeUrl)
+        if(robotRequest.status_code == 200):
+            result = json.loads(robotRequest.text)
+            IP_ADDRESS = result["data"]
+            match = re.search(r'^((25[0-5]|(2[0-4]|1\d|[1-9]|)\d)(\.(?!$)|$)){4}$', result["data"]) #regular expression for IP address
+            if(match == None):
+                return "False"
+            else:
+                return IP_ADDRESS #IP address found
+        else:
+            return "False"
+    except ConnectionError:
+        return "False"
+    except:
+        return "False"
+
+def getProtocols():
+    IP_ADDRESS = connectionCheck()
+    if(IP_ADDRESS == "False"):
+        return Response(json.dumps({'error': 'No connection to robot'}), status=200, mimetype=contentType)
+
+    url = urlStart + IP_ADDRESS + robotPORT + "/protocols"
+    headers = {"opentrons-version": "2", "Content-Type": contentType}
+
+    #robotProtocolRequest
+    try:
+        robotProtocolRequest = requests.request("GET", url, headers=headers)
+    except requests.exceptions.InvalidURL:
+        return Response(json.dumps({'error': 'No connection to robot'}), status=200, mimetype=contentType)
+    protocols = json.loads(robotProtocolRequest.text)
+
+    if(robotProtocolRequest.status_code == 200):
+        return protocols
+    
+    else:
+        return {}
+    
+def getRuns():
+    """Route to get list of protocols known to the robot."""
+
+    IP_ADDRESS = connectionCheck()
+    if(IP_ADDRESS == "False"):
+        return Response(json.dumps({'error': 'No connection to robot'}), status=200, mimetype=contentType)
+
+    url = urlStart + IP_ADDRESS + robotPORT + "/runs"
+    headers = {"opentrons-version": "2", "Content-Type": contentType}
+
+    #robotProtocolRequest
+    try:
+        robotRunsRequest = requests.request("GET", url, headers=headers)
+    except requests.exceptions.InvalidURL:
+        return Response(json.dumps({'error': 'No connection to robot'}), status=200, mimetype=contentType)
+    runs = json.loads(robotRunsRequest.text)
+
+    if(robotRunsRequest.status_code == 200):
+        return runs
+    else:
+        return {}
+    
+def postRun():
+    """Route to create a run for the robot."""
+    IP_ADDRESS = connectionCheck()
+    if(IP_ADDRESS == "False"):
+        return Response(json.dumps({'error': 'No connection to robot'}), status=200, mimetype=contentType)
+
+    try:
+        obj = request.get_data()
+        obj = json.loads(obj)
+        tempProtocolId = obj['protocol_id']
+    except KeyError:
+        return Response(json.dumps({'error': 'No id found in body request'}), status=200, mimetype=contentType)
+
+    url = urlStart + IP_ADDRESS + robotPORT + "/runs"
+    headers = {"opentrons-version": "2", "Content-Type": contentType}
+    payload = {
+        "data": {
+            "protocolId": "",
+            "labwareOffsets": [
+                {
+                    "definitionUri": "opentrons/opentrons_96_tiprack_300ul/1",
+                    "location": {
+                        "slotName": "1"
+                },
+                "vector": {
+                    "x": 0.29999999999999893,
+                    "y": 0.9000000000000057,
+                    "z": 0.0
+                    }
+                }
+            ]
+        }
+    }
+    payload["data"]["protocolId"] = tempProtocolId
+    payload = json.dumps(payload)
+
+    try:
+        robotRequest = requests.request("POST", url, headers=headers, data=payload)
+    except requests.exceptions.InvalidURL:
+        return Response(json.dumps({'error': 'No connection to robot'}), status=200, mimetype=contentType)
+    
+    if(robotRequest.status_code == 201):
+        createdRunId = robotRequest.json()["data"]["id"]
+        return Response(json.dumps({'message': 'Run created', 'runId': '{createdRunId}'.format(createdRunId=createdRunId)}), status=201, mimetype=contentType)
+    else:
+        return Response(json.dumps({'error': 'Run was not created'}), status=200, mimetype=contentType)
+    
+def getCurrentRun():
+    """Route to get the current run id"""
+    IP_ADDRESS = connectionCheck()
+    if(IP_ADDRESS == "False"):
+        return Response(json.dumps({'error': 'No connection to robot'}), status=200, mimetype=contentType)
+
+    runRequest = getRuns()
+
+    if (len(runRequest.keys()) == 0):
+        return Response(json.dumps({'error': 'No runs found'}), status=200, mimetype=contentType)
+
+    try:
+        tempCurrentRun = runRequest["links"]["current"]["href"]
+        tempCurrentRun = tempCurrentRun.split("/runs/")
+        currentRun = tempCurrentRun[1]
+    except KeyError: #this happens if there is no current run
+        currentRun = ""
+
+    return currentRun
+
+def getRunStatus():
+    """Route to get the status of the current run."""
+    IP_ADDRESS = connectionCheck()
+    if(IP_ADDRESS == "False"):
+        return Response(json.dumps({'error': 'No connection to robot'}), status=200, mimetype=contentType)
+
+    currentRunRequest = getCurrentRun()
+    if(len(currentRunRequest) == 0):
+        return Response(json.dumps({'error': 'No current run'}), status=200, mimetype=contentType)
+    currentRun = currentRunRequest 
+    url = urlStart + IP_ADDRESS + robotPORT + "/runs/" + currentRun
+    headers = {"opentrons-version": "2", "Content-Type": contentType}
+    
+    try:
+        robotRequest = requests.request("GET", url, headers=headers)
+    except requests.exceptions.InvalidURL:
+        return Response(json.dumps({'error': 'No IP to robot found'}), status=200, mimetype=contentType)
+    except requests.exceptions.ConnectionError:
+        return Response(json.dumps({'error': 'No connection to robot'}), status=200, mimetype=contentType)
+
+    if(robotRequest.status_code == 200):
+        try:
+            status = robotRequest.json()["data"]["status"]
+            return status
+        except KeyError:
+            return Response(json.dumps({'error': 'No current run'}), status=200, mimetype=contentType)
+    else:
+        return Response(json.dumps({'error': '{error}'.format(error=robotRequest)}), status=200, mimetype=contentType)
+
+def postRunAction():
+    """Route to run/resume, pause or stop a run."""
+    IP_ADDRESS = connectionCheck()
+    if(IP_ADDRESS == "False"):
+        return Response(json.dumps({'error': 'No connection to robot'}), status=200, mimetype=contentType)
+
+    try:
+        obj = request.get_data()
+        obj = json.loads(obj)
+        command = obj['command']
+    except KeyError:
+        return Response(json.dumps({'error': 'No command found'}), status=200, mimetype=contentType)
+    except requests.exceptions.ConnectionError:
+        return Response(json.dumps({'error': 'No connection to robot'}), status=200, mimetype=contentType)
+
+    if(getRunStatus() == "succeeded" or getRunStatus() == "stopped"):
+        return Response(json.dumps({'error': 'Run has already been executed'}), status=200, mimetype=contentType)
+
+    if(command == "play"):
+        payload = {
+            "data": {
+                "actionType": "play" 
+            }
+        }
+    elif(command == 'pause'):
+        payload = {
+            "data": {
+                "actionType": "pause" 
+            }
+        }
+    else:
+        payload = {
+            "data": {
+                "actionType": "stop" 
+            }
+        }
+    payload = json.dumps(payload)
+
+    currentRunRequest = getCurrentRun()
+    if(len(currentRunRequest) == 0):
+        return Response(json.dumps({'error': 'No current run'}), status=200, mimetype=contentType)
+    
+    currentRun = currentRunRequest
+
+    url = urlStart + IP_ADDRESS + robotPORT + "/runs/" + currentRun + "/actions"
+    headers = {"opentrons-version": "2", "Content-Type": contentType}
+    
+    try:
+        robotRequest = requests.request("POST", url, headers=headers, data=payload)
+    except requests.exceptions.InvalidURL:
+        return Response(json.dumps({'error': 'No connection to robot'}), status=200, mimetype=contentType)
+    
+    if(robotRequest.status_code == 201):
+        return Response(json.dumps({'message': 'Run {command}'.format(command=command)}), status=201, mimetype=contentType)
+    else:
+        return Response(json.dumps({'error': '{error}'.format(error=robotRequest.reason)}), status=200, mimetype=contentType)
